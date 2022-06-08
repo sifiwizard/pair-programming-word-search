1b6acd86fa41f1b47d437ab72574005a94e0faef
Aidan Prior <sifiwizard.code@gmail.com>
2022-06-08 14:42:17 -0600

    Vertical

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+    const newMatrix = [];
+    for (const col of matrix) {
+      for (const row in col) {
+        if (newMatrix[row]) {
+          newMatrix[row].push(col[row]);
+        } else {
+          newMatrix.push([col[row]]);
+        }
+      }
     }
-}
+    return newMatrix;
+  };
+
+const wordSearch = (letters, word) => { 
+  
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (let l of horizontalJoin) {
+        if (l.includes(word)) return true;
+    }
+    let newLetters = transpose(letters);
+
+    const verticleJoin = newLetters.map(ls => ls.join(''));
+    for (let l of verticleJoin) {
+        if (l.includes(word)) return true;
+    }
+    return false;
+};
 
 module.exports = wordSearch
+
